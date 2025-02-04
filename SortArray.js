bdd64fae063e8a473bd74961aab959f0e103b659
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-02-04 17:41:52 +0530

    Program to sort an Array

--- SortArray.js
+++ SortArray.js
@@ -1,8 +1,13 @@
+/*
+Description: Method to find the Factorial of Number using Recursion
+Parameter: number
+return: number
+*/
 function sortMyArray(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
             if (arr[i] > arr[j]) {
-                let temp = aar[i];
+                let temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
@@ -10,3 +15,5 @@ function sortMyArray(arr) {
     }
     return arr;
 }
+let intArray = [23, 12, 9, 56, 78, 45, 17];
+console.log("" + sortMyArray(intArray));
